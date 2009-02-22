ab90d3a0c025c78e71d9078388741a6637480acd
Aleksey Cheusov <vle@gmx.net>
2009-02-22 10:17:56 +0000

    oops, I forgot about it

---
+++ tests/test1.c
@@ -0,0 +1,14 @@
+#include <stdlib.h>
+
+int main ()
+{
+	void *p1 = NULL;
+	void *p2 = NULL;
+
+	p1 = malloc (555);
+	p2 = realloc (p2, 666);
+	p2 = realloc (p2, 777);
+	p2 = realloc (p2, 888);
+
+	free (p1);
+}
