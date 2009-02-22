1119c8f2c7bdce609dfc9abfd2d45ac283071b97
Aleksey Cheusov <vle@gmx.net>
2009-02-22 09:08:18 +0000

    Good test for lmdbg-run

--- tests/test.sh
+++ tests/test.sh
@@ -1,5 +1,7 @@
 #!/bin/sh
 
+export LC_ALL=C
+
 set -e
 
 unify_paths (){
@@ -11,7 +13,7 @@ runtest (){
     shift
 
     echo '--------------------------------------------------'
-    echo "------- args: $@" | unify_paths
+    echo "------- args: $prog $@" | unify_paths
     "$OBJDIR"/"$prog" "$@" 2>&1 | unify_paths
 }
 
@@ -24,6 +26,11 @@ unify_text (){
     awk -v num="$num" 'NR == num {$0="XXX"} {print}' "$@"
 }
 
+unify_address (){
+    sed 's,0x[0-9a-fA-F][0-9a-fA-F]*,0xF00DBEAF,g' "$@"
+}
+
+# stupid tests
 runtest lmdbg-run --help        | head -3 | unify_text 3
 runtest lmdbg-sym --help        | head -3 | unify_text 3
 runtest lmdbg-check --help      | head -3 | unify_text 3
@@ -43,3 +50,15 @@ runtest lmdbg-run -V               | head -3 | unify_text 3
 runtest lmdbg-sym -V               | head -3 | unify_text 3
 runtest lmdbg-check -V             | head -3 | unify_text 3
 runtest lmdbg-leak-check -V        | head -3 | unify_text 3
+
+# real tests
+execname="$OBJDIR"/_test1
+srcname="$SRCDIR"/tests/test1.c
+logname="$OBJDIR"/_log
+
+"$CC" -O0 -g -o "$execname" "$srcname"
+runtest lmdbg-run -o "$logname" "$execname"
+
+grep malloc  "$logname" | unify_address
+grep realloc "$logname" | unify_address
+grep free    "$logname" | unify_address
