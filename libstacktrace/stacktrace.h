c0a3841b84d12fab046595c048b9c65354427d33
Aleksey Cheusov <vle@gmx.net>
2009-05-10 10:11:47 +0000

    header is now friendly for C++ compilers

--- libstacktrace/stacktrace.h
+++ libstacktrace/stacktrace.h
@@ -1,6 +1,14 @@
 #ifndef _STACKTRACE_H_
 #define _STACKTRACE_H_
 
+#ifdef  __cplusplus
+extern "C" {
+#endif
+
 int stacktrace (void **buffer, int size);
+
+#ifdef  __cplusplus
+}
+#endif
 
 #endif // _STACKTRACE_H_
