1e46fba2afe4f769ed23ecde7c89ebc7e2583ab2
Aleksey Cheusov <vle@gmx.net>
2009-11-18 10:45:57 +0000

    Regression tests have been completely reimplemented

--- test.mk
+++ test.mk
@@ -9,17 +9,20 @@ test: all
 	SRCDIR=${.CURDIR}; \
 	CC='${CC}'; \
 	export PATH LMDBG_LIB OBJDIR SRCDIR CC; \
-	if ( set -e; cd ${.CURDIR}/tests; \
-	    ./test.sh > ${.OBJDIR}/_test.res || exit 0; \
-	    diff -u test.out ${.OBJDIR}/_test.res > ${.OBJDIR}/_test2.res; \
-	    grep -Ev '^[-+] ([?][?]:NNN|0xF00DBEAF)$$' \
-		${.OBJDIR}/_test2.res > ${.OBJDIR}/_test3.res; \
-	    grep -E '^[-+]([^+-]|$$)' ${.OBJDIR}/_test3.res > /dev/null; \
-	    ); \
-	then \
-	    echo '   failed'; \
-	    grep -Ev '^[-+] ([?][?]:NNN|0xF00DBEAF)$$' ${.OBJDIR}/_test2.res; \
-	    false; \
-	else \
-	    echo '   succeeded'; \
-	fi
+	cd ${.CURDIR}/tests; \
+	./test.sh #> ${.OBJDIR}/_test.res
+
+# 	if ( set -e; cd ${.CURDIR}/tests; \
+# 	    ./test.sh > ${.OBJDIR}/_test.res || exit 0; \
+# 	    diff -u test.out ${.OBJDIR}/_test.res > ${.OBJDIR}/_test2.res; \
+# 	    grep -Ev '^[-+] ([?][?]:NNN|0xF00DBEAF)$$' \
+# 		${.OBJDIR}/_test2.res > ${.OBJDIR}/_test3.res; \
+# 	    grep -E '^[-+]([^+-]|$$)' ${.OBJDIR}/_test3.res > /dev/null; \
+# 	    ); \
+# 	then \
+# 	    echo '   failed'; \
+# 	    grep -Ev '^[-+] ([?][?]:NNN|0xF00DBEAF)$$' ${.OBJDIR}/_test2.res; \
+# 	    false; \
+# 	else \
+# 	    echo '   succeeded'; \
+# 	fi
--- tests/test.sh
+++ tests/test.sh
@@ -25,20 +25,12 @@ runtest (){
     prog="$1"
     shift
 
-    echo '--------------------------------------------------'
-    echo "------- args: $prog $@" | unify_paths
+#    echo '--------------------------------------------------'
+#    echo "------- args: $prog $@" | unify_paths
     $prog "$@" 2>&1 | unify_paths
 }
 
 ####################
-
-unify_text (){
-    #
-    num=$1
-    shift
-
-    awk -v num="$num" 'NR == num {$0="XXX"} {print}' "$@"
-}
 
 unify_address (){
     # input:  malloc ( <size> ) --> 0x81234567
@@ -52,8 +44,12 @@ unify_address (){
 	$1 == "realloc"        {$8 = "0xF00DBEAF"}
 	$1 == "realloc" && $3 != "NULL" {$3 = "0xF00DBEAF"}
 	match($0, /^ [^ \t]+/) {$0 = " 0xF00DBEAF" substr($0, RSTART+RLENGTH)}
-	{print}
+	{ print }
     ' "$@"
+}
+
+skip_useless_addr (){
+    awk 'NF != 1 || $1 != "0xF00DBEAF"' "$@"
 }
 
 hide_lmdbg_code (){
@@ -78,33 +74,115 @@ progress (){
     fi
 }
 
+version2XXX (){
+    awk '{$2 = "XXX"; print}'
+}
+
+tmpfn1=/tmp/lmdbg.1.$$
+tmpfn2=/tmp/lmdbg.2.$$
+tmpfn3=/tmp/lmdbg.3.$$
+
+ex=0
+
+cmp (){
+    # $1 - progress message
+    # $2 - expected text
+    printf '%s... ' "$1" 1>&2
+
+    cat > "$tmpfn2"
+    printf '%s' "$2" > "$tmpfn1"
+
+    if diff -u "$tmpfn1" "$tmpfn2" > "$tmpfn3"; then
+	echo ok
+    else
+	echo FAILED
+	awk '{print "   " $0}' "$tmpfn3"
+	ex=1
+    fi
+}
+
 ####################
 # stupid tests
 progress "stupid tests..."
 
-runtest lmdbg-run --help        | head -3 | unify_text 3
-runtest lmdbg-sym --help        | head -3 | unify_text 3
-runtest lmdbg-leaks --help      | head -3 | unify_text 3
-runtest lmdbg-sysleaks --help   | head -3 | unify_text 3
+runtest lmdbg-run --help | head -1 |
+cmp "lmdbg-run --help" \
+'lmdbg-run is intended to run your program with
+'
 
-runtest lmdbg-run -h            | head -3 | unify_text 3
-runtest lmdbg-sym -h            | head -3 | unify_text 3
-runtest lmdbg-leaks -h          | head -3 | unify_text 3
-runtest lmdbg-sysleaks -h       | head -3 | unify_text 3
+runtest lmdbg-sym --help | head -1 |
+cmp "lmdbg-sym --help" \
+"lmdbg-sym analyses lmdbg-run's output and converts
+"
 
-runtest lmdbg-run --version        | head -3 | unify_text 3
-runtest lmdbg-sym --version        | head -3 | unify_text 3
-runtest lmdbg-leaks --version      | head -3 | unify_text 3
-runtest lmdbg-sysleaks --version   | head -3 | unify_text 3
+runtest lmdbg-leaks --help | head -1 |
+cmp "lmdbg-leaks --help" \
+"This program analyses lmdbg-run's or lmdbg-sym's output and
+"
 
-runtest lmdbg-run -V               | head -3 | unify_text 3
-runtest lmdbg-sym -V               | head -3 | unify_text 3
-runtest lmdbg-leaks -V             | head -3 | unify_text 3
-runtest lmdbg-sysleaks -V          | head -3 | unify_text 3
+runtest lmdbg-sysleaks --help   | head -1 |
+cmp "lmdbg-sysleaks --help" \
+"This program analyses lmdbg-run's output and
+"
 
-####################
-# real tests
-progress "real tests..."
+runtest lmdbg-run -h            | head -1 |
+cmp "lmdbg-run --help" \
+'lmdbg-run is intended to run your program with
+'
+
+runtest lmdbg-sym -h            | head -1 |
+cmp "lmdbg-sym --help" \
+"lmdbg-sym analyses lmdbg-run's output and converts
+"
+
+runtest lmdbg-leaks -h          | head -1 |
+cmp "lmdbg-leaks --help" \
+"This program analyses lmdbg-run's or lmdbg-sym's output and
+"
+
+runtest lmdbg-sysleaks -h       | head -1 |
+cmp "lmdbg-sysleaks --help" \
+"This program analyses lmdbg-run's output and
+"
+
+runtest lmdbg-run --version        | head -1 | version2XXX |
+cmp "lmdbg-run --version" \
+"lmdbg-run XXX
+"
+
+runtest lmdbg-sym --version        | head -1 | version2XXX |
+cmp "lmdbg-sym --version" \
+"lmdbg-sym XXX
+"
+
+runtest lmdbg-leaks --version      | head -1 | version2XXX |
+cmp "lmdbg-leaks --version" \
+"lmdbg-leaks XXX
+"
+runtest lmdbg-sysleaks --version   | head -1 | version2XXX |
+cmp "lmdbg-sysleaks --version" \
+"lmdbg-sysleaks XXX
+"
+
+runtest lmdbg-run -V               | head -1 | version2XXX |
+cmp "lmdbg-run -V" \
+"lmdbg-run XXX
+"
+
+runtest lmdbg-sym -V               | head -1 | version2XXX |
+cmp "lmdbg-sym -V" \
+"lmdbg-sym XXX
+"
+
+runtest lmdbg-leaks -V             | head -1 | version2XXX |
+cmp "lmdbg-leaks -V" \
+"lmdbg-leaks XXX
+"
+
+runtest lmdbg-sysleaks -V          | head -1 | version2XXX |
+cmp "lmdbg-sysleaks -V" \
+"lmdbg-sysleaks XXX
+"
 
 # test1.c
 execname="$OBJDIR"/_test1
@@ -127,68 +205,110 @@ LD_LIBRARY_PATH=$OBJDIR
 export LD_LIBRARY_PATH
 
 # -o
-progress "test lmdbg-run -o..."
-
 runtest lmdbg-run -o "$logname" "$execname"
 
-grep malloc  "$logname" | unify_address
-grep realloc "$logname" | unify_address
-grep free    "$logname" | unify_address
-# --log
-progress "test lmdbg-run --log..."
+unify_address "$logname" | skip_useless_addr |
+cmp "test1.c: lmdbg-run -o" \
+"malloc ( 555 ) --> 0xF00DBEAF
+realloc ( NULL , 666 ) --> 0xF00DBEAF
+realloc ( 0xF00DBEAF , 777 ) --> 0xF00DBEAF
+realloc ( 0xF00DBEAF , 888 ) --> 0xF00DBEAF
+free ( 0xF00DBEAF )
+"
 
+# --log
 runtest lmdbg-run --log "$logname" "$execname"
 
-grep malloc  "$logname" | unify_address
-grep realloc "$logname" | unify_address
-grep free    "$logname" | unify_address
+unify_address "$logname" | skip_useless_addr |
+cmp "test1.c: lmdbg-run --log" \
+"malloc ( 555 ) --> 0xF00DBEAF
+realloc ( NULL , 666 ) --> 0xF00DBEAF
+realloc ( 0xF00DBEAF , 777 ) --> 0xF00DBEAF
+realloc ( 0xF00DBEAF , 888 ) --> 0xF00DBEAF
+free ( 0xF00DBEAF )
+"
 
 # lmdbg-leaks
-progress "test lmdbg-leaks ..."
-
 logname2="$OBJDIR"/_log2
 runtest lmdbg-leaks "$logname" > "$logname2"
 
-grep -- --- "$logname2"
-
-grep malloc  "$logname2" | unify_address
-grep realloc "$logname2" | unify_address
-grep free    "$logname2" | unify_address
+unify_address "$logname2" | skip_useless_addr |
+cmp "test1.c: lmdbg-leaks" \
+"realloc ( 0xF00DBEAF , 888 ) --> 0xF00DBEAF
+"
 
 # lmdbg-sym --with-gdb
-progress "test lmdbg-sym --with-gdb..."
-
 runtest lmdbg-sym --with-gdb "$execname" "$logname" |
 unify_address | hide_lmdbg_code | hide_line_numbers |
-canonize_paths
+canonize_paths | skip_useless_addr |
+cmp "test1.c: lmdbg-sym" \
+"malloc ( 555 ) --> 0xF00DBEAF
+ 0xF00DBEAF	test1.c:NNN	main
+realloc ( NULL , 666 ) --> 0xF00DBEAF
+ 0xF00DBEAF	test1.c:NNN	main
+realloc ( 0xF00DBEAF , 777 ) --> 0xF00DBEAF
+ 0xF00DBEAF	test1.c:NNN	main
+realloc ( 0xF00DBEAF , 888 ) --> 0xF00DBEAF
+ 0xF00DBEAF	test1.c:NNN	main
+free ( 0xF00DBEAF )
+ 0xF00DBEAF	test1.c:NNN	main
+"
 
 # lmdbg-sym -g
-progress "test lmdbg-sym -g..."
-
 runtest lmdbg-sym -g "$execname" "$logname" |
 unify_address | hide_lmdbg_code | hide_line_numbers |
-canonize_paths
+canonize_paths | skip_useless_addr |
+cmp "test1.c: lmdbg-sym -g" \
+"malloc ( 555 ) --> 0xF00DBEAF
+ 0xF00DBEAF	test1.c:NNN	main
+realloc ( NULL , 666 ) --> 0xF00DBEAF
+ 0xF00DBEAF	test1.c:NNN	main
+realloc ( 0xF00DBEAF , 777 ) --> 0xF00DBEAF
+ 0xF00DBEAF	test1.c:NNN	main
+realloc ( 0xF00DBEAF , 888 ) --> 0xF00DBEAF
+ 0xF00DBEAF	test1.c:NNN	main
+free ( 0xF00DBEAF )
+ 0xF00DBEAF	test1.c:NNN	main
+"
 
 # lmdbg-sym -a
 progress "test lmdbg-sym -a..."
 
 runtest lmdbg-sym -a "$execname" "$logname" |
 unify_address | hide_lmdbg_code | hide_line_numbers |
-canonize_paths
+canonize_paths | skip_useless_addr |
+cmp "test1.c: lmdbg-sym -a" \
+"malloc ( 555 ) --> 0xF00DBEAF
+ 0xF00DBEAF	test1.c:NNN
+realloc ( NULL , 666 ) --> 0xF00DBEAF
+ 0xF00DBEAF	test1.c:NNN
+realloc ( 0xF00DBEAF , 777 ) --> 0xF00DBEAF
+ 0xF00DBEAF	test1.c:NNN
+realloc ( 0xF00DBEAF , 888 ) --> 0xF00DBEAF
+ 0xF00DBEAF	test1.c:NNN
+free ( 0xF00DBEAF )
+ 0xF00DBEAF	test1.c:NNN
+"
 
 # lmdbg-run --pipe lmdbg-leaks
 runtest lmdbg-run -o "$logname" --pipe lmdbg-leaks "$execname"
 
-grep malloc  "$logname" | unify_address
-grep realloc "$logname" | unify_address
-grep free    "$logname" | unify_address
+unify_address "$logname" | hide_lmdbg_code |
+hide_line_numbers |
+canonize_paths | skip_useless_addr |
+cmp "test1.c: lmdbg-run --pipe lmdbg-leaks" \
+"realloc ( 0xF00DBEAF , 888 ) --> 0xF00DBEAF
+"
 
 # lmdbg-run -p lmdbg-leaks
 runtest lmdbg-run -o "$logname" -p lmdbg-leaks "$execname"
 
-grep malloc  "$logname" | unify_address
-grep realloc "$logname" | unify_address
-grep free    "$logname" | unify_address
+unify_address "$logname" | hide_lmdbg_code |
+hide_line_numbers |
+canonize_paths | skip_useless_addr |
+cmp "test1.c: lmdbg-run -p lmdbg-leaks" \
+"realloc ( 0xF00DBEAF , 888 ) --> 0xF00DBEAF
+"
 
 # test1.c
 execname="$OBJDIR"/_test2
@@ -200,47 +320,80 @@ $CC -O0 -g -o "$execname" "$srcname"
 # lmdbg-run -o with two leaks
 runtest lmdbg-run -o "$logname" -p "lmdbg-sym $execname" "$execname"
 
-unify_paths_inplace "$logname"
-
-grep '^malloc'  "$logname" | unify_address
-grep '^realloc' "$logname" | unify_address
-grep '^free'    "$logname" | unify_address
+unify_address "$logname" | skip_useless_addr |
+hide_line_numbers |
+canonize_paths | skip_useless_addr |
+cmp "test2.c: lmdbg-run -p" \
+"malloc ( 555 ) --> 0xF00DBEAF
+ 0xF00DBEAF	lmdbg.c:NNN	log_stacktrace
+ 0xF00DBEAF	lmdbg.c:NNN	malloc
+ 0xF00DBEAF	test2.c:NNN	main
+realloc ( NULL , 666 ) --> 0xF00DBEAF
+ 0xF00DBEAF	lmdbg.c:NNN	log_stacktrace
+ 0xF00DBEAF	lmdbg.c:NNN	realloc
+ 0xF00DBEAF	test2.c:NNN	main
+realloc ( 0xF00DBEAF , 777 ) --> 0xF00DBEAF
+ 0xF00DBEAF	lmdbg.c:NNN	log_stacktrace
+ 0xF00DBEAF	lmdbg.c:NNN	realloc
+ 0xF00DBEAF	test2.c:NNN	main
+realloc ( 0xF00DBEAF , 888 ) --> 0xF00DBEAF
+ 0xF00DBEAF	lmdbg.c:NNN	log_stacktrace
+ 0xF00DBEAF	lmdbg.c:NNN	realloc
+ 0xF00DBEAF	test2.c:NNN	main
+"
 
 # lmdbg-leaks with two leaks
-progress "test lmdbg-leaks..."
-
 logname2="$OBJDIR"/_log2
 runtest lmdbg-leaks "$logname" > "$logname2"
 
-grep -- --- "$logname2"
+unify_paths_inplace "$logname"
 
-grep '^malloc'  "$logname2" | unify_address
-grep '^realloc' "$logname2" | unify_address
-grep '^free'    "$logname2" | unify_address
+unify_address "$logname2" | skip_useless_addr |
+hide_line_numbers |
+cmp "test2.c: lmdbg-run -p again" \
+"realloc ( 0xF00DBEAF , 888 ) --> 0xF00DBEAF
+ 0xF00DBEAF	lmdbg.c:NNN	log_stacktrace
+ 0xF00DBEAF	lmdbg.c:NNN	realloc
+ 0xF00DBEAF	/lmdbg/dir/test2.c:NNN	main
+malloc ( 555 ) --> 0xF00DBEAF
+ 0xF00DBEAF	lmdbg.c:NNN	log_stacktrace
+ 0xF00DBEAF	lmdbg.c:NNN	malloc
+ 0xF00DBEAF	/lmdbg/dir/test2.c:NNN	main
+"
 
 # lmdbg-leaks with lmdbg-leak1.conf
-progress "test lmdbg-sysleaks 1..."
-
 runtest lmdbg-sysleaks -c ./lmdbg1.conf -s \
     "$logname" > "$logname2"
 
-grep -- --- "$logname2"
-
-grep '^malloc'  "$logname2" | unify_address
-grep '^realloc' "$logname2" | unify_address
-grep '^free'    "$logname2" | unify_address
+unify_address "$logname2" | skip_useless_addr |
+hide_line_numbers |
+cmp "test2.c: lmdbg-sysleaks -c ./lmdbg1.conf" \
+"malloc ( 555 ) --> 0xF00DBEAF
+ 0xF00DBEAF	lmdbg.c:NNN	log_stacktrace
+ 0xF00DBEAF	lmdbg.c:NNN	malloc
+ 0xF00DBEAF	/lmdbg/dir/test2.c:NNN	main
+realloc ( NULL , 666 ) --> 0xF00DBEAF
+ 0xF00DBEAF	lmdbg.c:NNN	log_stacktrace
+ 0xF00DBEAF	lmdbg.c:NNN	realloc
+ 0xF00DBEAF	/lmdbg/dir/test2.c:NNN	main
+realloc ( 0xF00DBEAF , 777 ) --> 0xF00DBEAF
+ 0xF00DBEAF	lmdbg.c:NNN	log_stacktrace
+ 0xF00DBEAF	lmdbg.c:NNN	realloc
+ 0xF00DBEAF	/lmdbg/dir/test2.c:NNN	main
+realloc ( 0xF00DBEAF , 888 ) --> 0xF00DBEAF
+ 0xF00DBEAF	lmdbg.c:NNN	log_stacktrace
+ 0xF00DBEAF	lmdbg.c:NNN	realloc
+ 0xF00DBEAF	/lmdbg/dir/test2.c:NNN	main
+"
 
 # lmdbg-leaks with lmdbg-leak2.conf
-progress "test lmdbg-sysleaks 2..."
-
 runtest lmdbg-sysleaks -c ./lmdbg2.conf -s \
     "$logname" > "$logname2"
 
-grep -- --- "$logname2"
-
-grep '^malloc'  "$logname2" | unify_address
-grep '^realloc' "$logname2" | unify_address
-grep '^free'    "$logname2" | unify_address
+unify_address "$logname2" | skip_useless_addr |
+hide_line_numbers |
+canonize_paths |
+cmp "test2.c: lmdbg-sysleaks -c ./lmdbg2.conf -s" ''
 
 # lmdbg-leaks with lmdbg-leak3.conf
 progress "test lmdbg-sysleaks 3..."
@@ -248,67 +401,95 @@ progress "test lmdbg-sysleaks 3..."
 runtest lmdbg-sysleaks -c ./lmdbg3.conf -s \
     "$logname" > "$logname2"
 
-grep -- --- "$logname2"
-
-grep '^malloc'  "$logname2" | unify_address
-grep '^realloc' "$logname2" | unify_address
-grep '^free'    "$logname2" | unify_address
+unify_address "$logname2" | skip_useless_addr |
+hide_line_numbers |
+cmp "test2.c: lmdbg-sysleaks -c ./lmdbg3.conf -s" \
+'malloc ( 555 ) --> 0xF00DBEAF
+ 0xF00DBEAF	lmdbg.c:NNN	log_stacktrace
+ 0xF00DBEAF	lmdbg.c:NNN	malloc
+ 0xF00DBEAF	/lmdbg/dir/test2.c:NNN	main
+realloc ( NULL , 666 ) --> 0xF00DBEAF
+ 0xF00DBEAF	lmdbg.c:NNN	log_stacktrace
+ 0xF00DBEAF	lmdbg.c:NNN	realloc
+ 0xF00DBEAF	/lmdbg/dir/test2.c:NNN	main
+realloc ( 0xF00DBEAF , 777 ) --> 0xF00DBEAF
+ 0xF00DBEAF	lmdbg.c:NNN	log_stacktrace
+ 0xF00DBEAF	lmdbg.c:NNN	realloc
+ 0xF00DBEAF	/lmdbg/dir/test2.c:NNN	main
+'
 
 # lmdbg-leaks with lmdbg-leak3.conf
-progress "test lmdbg-sysleaks 4..."
-
 runtest lmdbg-sysleaks -c ./lmdbg3.conf \
     "$logname" > "$logname2"
 
-grep -- --- "$logname2"
-
-grep '^malloc'  "$logname2" | unify_address
-grep '^realloc' "$logname2" | unify_address
-grep '^free'    "$logname2" | unify_address
-
-# lmdbg!
-progress "test lmdbg 1..."
-
-runtest lmdbg -c ./lmdbg3.conf -o "$logname" "$OBJDIR"/_test1 \
-    > "$logname2"
-
-grep -- --- "$logname2"
-
-grep '^malloc'  "$logname" | unify_address
-grep '^realloc' "$logname" | unify_address
-grep '^free'    "$logname" | unify_address
+unify_address "$logname2" | skip_useless_addr |
+hide_line_numbers |
+cmp "test2.c: lmdbg-sysleaks -c ./lmdbg3.conf" \
+'realloc ( 0xF00DBEAF , 888 ) --> 0xF00DBEAF
+ 0xF00DBEAF	lmdbg.c:NNN	log_stacktrace
+ 0xF00DBEAF	lmdbg.c:NNN	realloc
+ 0xF00DBEAF	/lmdbg/dir/test2.c:NNN	main
+'
 
 # lmdbg!
-progress "test lmdbg 2..."
-runtest lmdbg -v -c ./lmdbg5.conf -o "$logname" "$OBJDIR"/_test1
+runtest lmdbg -c ./lmdbg3.conf -o "$logname" "$OBJDIR"/_test1
+
+unify_address "$logname" | skip_useless_addr |
+hide_line_numbers | unify_paths |
+cmp "test1.c: lmdbg -o" \
+'realloc ( 0xF00DBEAF , 888 ) --> 0xF00DBEAF
+ 0xF00DBEAF	lmdbg.c:NNN	log_stacktrace
+ 0xF00DBEAF	lmdbg.c:NNN	realloc
+ 0xF00DBEAF	/lmdbg/dir/test1.c:NNN	main
+'
+
+# lmdbg!
+runtest lmdbg -v -c ./lmdbg5.conf -o "$logname" "$OBJDIR"/_test1 1>"$logname2"
+
+cat "$logname2" |
+cmp "test1.c: lmdbg -v -c lmdbg5.conf" \
+'No memory leaks detected
+'
 
 # lmdbg!
 progress "test lmdbg 3..."
 
-runtest lmdbg -c ./lmdbg6.conf -o "$logname" "$OBJDIR"/_test2 \
-    > "$logname2"
+runtest lmdbg -c ./lmdbg6.conf -o "$logname" "$OBJDIR"/_test2
 
-grep -- --- "$logname2"
-
-grep '^malloc'  "$logname" | unify_address
-grep '^realloc' "$logname" | unify_address
-grep '^free'    "$logname" | unify_address
+unify_address "$logname" | skip_useless_addr |
+hide_line_numbers | unify_paths |
+cmp "test1.c: lmdbg -o" \
+'malloc ( 555 ) --> 0xF00DBEAF
+ 0xF00DBEAF	lmdbg.c:NNN	log_stacktrace
+ 0xF00DBEAF	lmdbg.c:NNN	malloc
+ 0xF00DBEAF	/lmdbg/dir/test2.c:NNN	main
+'
 
 # lmdbg-run -o and shared libraries
 progress "test lmdbg-run -o $exec3name..."
 
 runtest lmdbg-run -o "$logname" "$exec3name"
 
-grep malloc  "$logname" | unify_address
-grep realloc "$logname" | unify_address
-grep free    "$logname" | unify_address
+unify_address "$logname" | skip_useless_addr |
+hide_line_numbers | unify_paths |
+cmp "test3.c: lmdbg-run -o" \
+'malloc ( 555 ) --> 0xF00DBEAF
+malloc ( 666 ) --> 0xF00DBEAF
+'
 
 # lmdbg-sym -g and shared libraries
 progress "test lmdbg-sym -g $exec3name..."
 
 runtest lmdbg-sym --with-gdb "$exec3name" "$logname" |
 unify_address | hide_lmdbg_code | hide_line_numbers |
-canonize_paths
+skip_useless_addr |
+cmp "test3.c: lmdbg-sym -g" \
+'malloc ( 555 ) --> 0xF00DBEAF
+ 0xF00DBEAF	/lmdbg/dir/libtest.c:NNN	allocate_memory
+ 0xF00DBEAF	/lmdbg/dir/test3.c:NNN	main
+malloc ( 666 ) --> 0xF00DBEAF
+ 0xF00DBEAF	/lmdbg/dir/test3.c:NNN	main
+'
 
 # lmdbg-run + test4.c
 progress "test lmdbg-run with calloc(3)"
@@ -318,12 +499,24 @@ logname="$OBJDIR"/_log
 
 $CC -O0 -g -o "$execname" "$srcname"
 runtest lmdbg-run -o "$logname" "$execname"
-grep -E '[cm]alloc|realloc|free|posix_memalign'  "$logname" | unify_address
+
+unify_address "$logname" |
+skip_useless_addr |
+cmp "test4.c: lmdbg-run -o" \
+'calloc ( 555 , 16 ) --> 0xF00DBEAF
+calloc ( 5 , 256 ) --> 0xF00DBEAF
+realloc ( 0xF00DBEAF , 1024 ) --> 0xF00DBEAF
+calloc ( 1 , 10240 ) --> 0xF00DBEAF
+free ( 0xF00DBEAF )
+'
 
 # lmdbg-leaks + test4.c
-progress "test lmdbg-leak with calloc(3)"
 runtest lmdbg-leaks "$logname" |
-unify_address
+unify_address | skip_useless_addr |
+cmp "test4.c: lmdbg-leaks + calloc" \
+'realloc ( 0xF00DBEAF , 1024 ) --> 0xF00DBEAF
+calloc ( 1 , 10240 ) --> 0xF00DBEAF
+'
 
 # lmdbg-run + test5.c
 progress "test lmdbg-run with posix_memalign(3)"
@@ -333,10 +526,31 @@ logname="$OBJDIR"/_log
 
 $CC -O0 -g -o "$execname" "$srcname"
 runtest lmdbg-run -o "$logname" "$execname"
-grep -E '[cm]alloc|realloc|free|posix_memalign'  "$logname" | unify_address
+
+unify_address "$logname" |
+skip_useless_addr |
+cmp "test5.c: lmdbg-run -o" \
+'posix_memalign ( 16 , 200 ) --> 0xF00DBEAF
+posix_memalign ( 8 , 256 ) --> 0xF00DBEAF
+realloc ( 0xF00DBEAF , 1024 ) --> 0xF00DBEAF
+posix_memalign ( 256 , 10240 ) --> 0xF00DBEAF
+free ( 0xF00DBEAF )
+'
 
 # lmdbg-leaks + test5.c
-progress "test lmdbg-leak with posix_memalign(3)"
 runtest lmdbg-leaks "$logname" |
-unify_address
+lmdbg-sym "$execname" |
+unify_paths | hide_line_numbers |
+unify_address | skip_useless_addr |
+cmp "test5.c: lmdbg-leaks + lmdbg-sym" \
+'realloc ( 0xF00DBEAF , 1024 ) --> 0xF00DBEAF
+ 0xF00DBEAF	lmdbg.c:NNN	log_stacktrace
+ 0xF00DBEAF	lmdbg.c:NNN	realloc
+ 0xF00DBEAF	/lmdbg/dir/test5.c:NNN	main
+posix_memalign ( 256 , 10240 ) --> 0xF00DBEAF
+ 0xF00DBEAF	lmdbg.c:NNN	log_stacktrace
+ 0xF00DBEAF	lmdbg.c:NNN	posix_memalign
+ 0xF00DBEAF	/lmdbg/dir/test5.c:NNN	main
+'
 
+exit "$ex"
