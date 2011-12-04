e68fd6b7ee871430c83bc6803e1167b01c3c067c
Aleksey Cheusov <vle@gmx.net>
2011-12-04 17:26:31 +0000

    Remove signal handlers and other garbage.    All we need is __builtin_frame_address and __builtin_return_address    builtins and nothing else.

--- libstacktrace/stacktrace.c
+++ libstacktrace/stacktrace.c
@@ -39,110 +39,54 @@ int stacktrace (void **buffer, int size)
 /* !HAVE_HEADER_EXECINFO_H (probably NetBSD/FreeBSD/Solaris etc.) */
 
 #include <string.h>
-#include <signal.h>
-#include <setjmp.h>
 
-static struct sigaction sigsegv_orig_handler;
-static struct sigaction  sigbus_orig_handler;
-
-static jmp_buf jmpbuf;
-
-static void handler_sigfatal (int dummy)
-{
-	longjmp (jmpbuf,1);
-}
-
-static void set_sigfatal_handlers (void)
-{
-	struct sigaction sa;
-
-	sa.sa_handler = handler_sigfatal;
-	sigemptyset (&sa.sa_mask);
-	sa.sa_flags = 0;
-
-	sigaction (SIGSEGV, &sa, &sigsegv_orig_handler);
-	sigaction (SIGBUS,  &sa,  &sigbus_orig_handler);
-}
-
-static void restore_sigfatal_handlers (void)
-{
-	sigaction (SIGSEGV, &sigsegv_orig_handler, NULL);
-	sigaction (SIGBUS,  &sigbus_orig_handler, NULL);
-}
-
-#define one_return_address(x)                  \
-		if (x >= size) break;                  \
-		tb [x] = __builtin_return_address (x); \
-		frame  = __builtin_frame_address (x); \
-		if (!tb [x] || !frame){\
-			tb [x] = NULL; \
-			break;\
-		}
+#define one_return_address(x)                      \
+		if (x == size) return size;                \
+		if (!__builtin_frame_address (x)) return x;\
+		tb [x] = __builtin_return_address (x);
 
 int stacktrace (void **tb, int size)
 {
-	int i  = 0;
-	void* frame = NULL;
-
-	for (i=0; i < size; ++i){
-		tb [i] = NULL;
-	}
-
-	set_sigfatal_handlers ();
-
-	if (!setjmp (jmpbuf)){
-		while (1){
-			one_return_address(0);
-			one_return_address(1);
-			one_return_address(2);
-			one_return_address(3);
-			one_return_address(4);
-			one_return_address(5);
-			one_return_address(6);
-			one_return_address(7);
-			one_return_address(8);
-			one_return_address(9);
-			one_return_address(10);
-			one_return_address(11);
-			one_return_address(12);
-			one_return_address(13);
-			one_return_address(14);
-			one_return_address(15);
-			one_return_address(16);
-			one_return_address(17);
-			one_return_address(18);
-			one_return_address(19);
-			one_return_address(20);
-			one_return_address(21);
-			one_return_address(22);
-			one_return_address(23);
-			one_return_address(24);
-			one_return_address(25);
-			one_return_address(26);
-			one_return_address(27);
-			one_return_address(28);
-			one_return_address(29);
-			one_return_address(30);
-			one_return_address(31);
-			one_return_address(32);
-			one_return_address(33);
-			one_return_address(34);
-			one_return_address(35);
-			one_return_address(36);
-			one_return_address(37);
-			one_return_address(38);
-			one_return_address(39);
-		}
-
-		longjmp (jmpbuf, 2);
-	}
-
-	restore_sigfatal_handlers ();
-
-	for (i=0; i < size; ++i){
-		if (!tb [i])
-			return i;
-	}
-	return size;
+	one_return_address(0);
+	one_return_address(1);
+	one_return_address(2);
+	one_return_address(3);
+	one_return_address(4);
+	one_return_address(5);
+	one_return_address(6);
+	one_return_address(7);
+	one_return_address(8);
+	one_return_address(9);
+	one_return_address(10);
+	one_return_address(11);
+	one_return_address(12);
+	one_return_address(13);
+	one_return_address(14);
+	one_return_address(15);
+	one_return_address(16);
+	one_return_address(17);
+	one_return_address(18);
+	one_return_address(19);
+	one_return_address(20);
+	one_return_address(21);
+	one_return_address(22);
+	one_return_address(23);
+	one_return_address(24);
+	one_return_address(25);
+	one_return_address(26);
+	one_return_address(27);
+	one_return_address(28);
+	one_return_address(29);
+	one_return_address(30);
+	one_return_address(31);
+	one_return_address(32);
+	one_return_address(33);
+	one_return_address(34);
+	one_return_address(35);
+	one_return_address(36);
+	one_return_address(37);
+	one_return_address(38);
+	one_return_address(39);
+	return 40;
 }
 #endif /* HAVE_HEADER_EXECINFO_H */
