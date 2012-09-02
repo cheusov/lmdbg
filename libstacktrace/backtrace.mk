5d394d56f8c9ee5d083e7d9739b93418f53f453d
Aleksey Cheusov <vle@gmx.net>
2012-09-02 16:18:57 +0300

    Avoid use of deprecated DPLIBS, minor clean-ups in libexecinfo support

--- libstacktrace/backtrace.mk
+++ libstacktrace/backtrace.mk
@@ -1,10 +1,15 @@
+MKC_NOAUTO_FUNCLIBS =	1
+
 MKC_CHECK_HEADERS +=	execinfo.h
 MKC_CHECK_FUNCLIBS +=	backtrace:execinfo
 
 .include <mkc.configure.mk>
 
 .if ${HAVE_HEADER.execinfo_h:U0}
-.if ${HAVE_FUNCLIB.backtrace:U0} || ${HAVE_FUNCLIB.backtrace.execinfo:U0}
 CFLAGS +=	-DEXTERNAL_BACKTRACE
+.if ${HAVE_FUNCLIB.backtrace.execinfo:U0}
+LDADD  +=	-lexecinfo
 .endif
 .endif
+
+.undef MKC_NOAUTO_FUNCLIBS
